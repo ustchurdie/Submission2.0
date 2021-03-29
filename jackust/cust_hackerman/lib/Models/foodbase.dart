95fa6f01a89bbbbd109a67d685df1bd744e3ad96
MHYeung <johnyeung1998@gmail.com>
2021-03-30 00:58:56 +0800

    added foodbase class

---
+++ jackust/cust_hackerman/lib/Models/foodbase.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FoodModel {
+    final String name; //name of the dish e.g. "Eggtart"
+    final String description; // description of the dish
+    final double calories; // calories of the dish
+    final String unit; //"打,件,條 etc." translations
+    final Image photo; //photo to show the brief outlook of the dish
+    final List<String> ingredients; //what does the dish included (may further elaborate to allergies/religion/seafood/vegan)
+    final Map<String, double> taste; //Different level of taste; using star/bar chart to present
+
+  FoodModel(this.ingredients, this.name, this.description, this.calories, this.unit, this.photo, this.taste);
+}
