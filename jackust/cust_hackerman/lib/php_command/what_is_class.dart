990ad46728dbcda0d4f9adfc77a7b956ae296736
MHYeung <johnyeung1998@gmail.com>
2021-04-16 20:01:26 +0800

    json

--- jackust/cust_hackerman/lib/php_command/what_is_class.dart
+++ jackust/cust_hackerman/lib/php_command/what_is_class.dart
@@ -27,22 +27,18 @@ class Food{
   Food({this.id,this.name, this.foodDescription, this.imageUrl});
 
   factory Food.fromJson(Map<String, dynamic> json){
-
     return Food(
-      id: json['id'] as String,
-      name: json['name'] as String,
-      foodDescription: json['food_description'] as String,
-      imageUrl: json['image_url'] as String,
+      id: json['id'],
+      name: json['name'],
+      foodDescription: json['food_description'],
+      imageUrl: json['image_url'],
     );
   }
-  Map<String, dynamic> toJson(){
-  return {
-    "id": this.id,
-    "name": this.name,
-    "food_description": this.foodDescription,
-    "image_url": this.imageUrl
-  };
 }
+
+List<Food> parseFood(String responseBody){
+  final parsed = jsonDecode(responseBody).cast<Map<String,dynamic>>();
+  return parsed.map<Food>((json) => Food.fromJson(json)).toList();
 }
 
 Future<List<Food>> downloadJSON() async {
@@ -57,11 +53,9 @@ Future<List<Food>> downloadJSON() async {
     if (statusCode == 200) {
       print ("connect successful");
       // If the API call to the server was successful, parse the course list in JSON format
-      print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
       //List foods= json.decode(response.body);
-      List foods= json.decode([{"id":"3","name":"sandwitch","foodDescription":"very yummy sandwitch","imageUrl":"sandwitch.jpg"},{"id":"2","name":"bun","foodDescription":"very yummy bun","imageUrl":"bun.jpg"},{"id":"1","name":"toast","foodDescription":"Very delicious and yummy","imageUrl":"toast.jpg"}]);
-      return foods.map((food) => new Food.fromJson(food)).toList();
+      print(response.body);
+      return parseFood(response.body);
     } else {
       // If the API call was fail, throw an error.
       throw Exception(
