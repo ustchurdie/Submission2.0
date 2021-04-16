f1bd7edbf6a7065438b9f77040a31327153dcd61
radam <samsonlui8685@gmail.com>
2021-04-16 22:47:45 +0800

    adjustment in get_specific.php

--- jackust/cust_hackerman/lib/php_command/get_specific.php
+++ jackust/cust_hackerman/lib/php_command/get_specific.php
@@ -7,14 +7,14 @@ header('Content-Type: application/json');
 $host='localhost';
 $user='root';
 $pass='samhui1234';
-$db='my_person_contacts';
+$db='food';
 
 $final=array(); $response=array();
 $con=mysqli_connect($host, $user, $pass, $db);
-if($con) 
+/*if($con) 
     echo 'connected successfully to mydb database <br>';
 else 
-    echo 'connection failed'. mysqli_error($con);
+    echo 'connection failed'. mysqli_error($con);*/
     
 if (isset($_POST['index'])){ //gets specific info from flutter request
     $sql_stmt  = $_POST['index'];
@@ -30,19 +30,18 @@ if (isset($_POST['index'])){ //gets specific info from flutter request
     $i=0; 
     if ($rows){
         while($row = mysqli_fetch_array($result)) {         
-            array_push($response, array("id"=>$row['id'], "name"=>$row['full_names'], 
-                    "gender"=>$row['gender'], "contact"=>$row['contact_no'], "email"=>$row['email'], 
-                    "city"=>$row['city'], "country"=>$row['country']));
-	    } 
+            array_push($response, array("id"=>$row['1'], "name"=>$row['name'], 
+                    "foodDescription"=>$row['food_description'], "imageUrl"=>$row['image_url']));
+	    }
     } 
     //$response['result']=array_values($final);
-    echo json_encode($response);    
+    echo json_encode(array_reverse($response));    
 }
 else echo "cannot find anything";
 
 mysqli_close($con);
  // tell browser that its a json data
  //converting array to JSON string
-return json_encode($response); 
+//return json_encode($response); 
 
 ?>
